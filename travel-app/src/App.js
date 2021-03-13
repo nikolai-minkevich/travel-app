34cec7142419ab016e0fb3eecd5e82248d6ff986
Nikolai Minkevich <minkevich.nikolai@yandex.ru>
2021-03-13 12:52:08 +0300

    fix: fix warnings

--- travel-app/src/App.js
+++ travel-app/src/App.js
@@ -5,18 +5,21 @@ import CountriesList from "./Components/CountriesList";
 import TravelAppAPI from "./Utils/TravelAppAPI";
 import React from "react";
 
-import Footer from './Components/Footer/Footer';
+import Footer from "./Components/Footer/Footer";
 
 class App extends React.PureComponent {
   constructor() {
     super();
     this.travelAppAPI = new TravelAppAPI();
-    this.loadData();
   }
 
   state = {
     countries: [],
   };
+
+  componentDidMount() {
+    this.loadData();
+  }
 
   loadData = async function () {
     const countries = await this.travelAppAPI.getCountries();
@@ -24,33 +27,26 @@ class App extends React.PureComponent {
      * name и capital будет принимать информацию в виде
      * name={country.capital}
      */
-    // let countriesList = (
 
-    // );
-
-    this.setState(
-      {
-        countries: countries,
-      },
-      console.log(this.state)
-    );
+    this.setState({
+      countries: countries,
+    });
   };
   render() {
     const { countries } = this.state;
     return (
-
       <React.Fragment>
         <div className="App">
           <Test />
           <CountriesList>
-          {countries.map((country) => {
-            return <CountryCard imageURL={country.coverURL} name={country.info[0].name} capital={country.info[0].capital} />;
-          }) || "Data is loading..."}
-        </CountriesList>
+            {countries.length === 0 ? "Data is loading..." : null}
+            {countries.map((country, index) => {
+              return <CountryCard imageURL={country.coverURL} name={country.info[0].name} capital={country.info[0].capital} key={index} />;
+            })}
+          </CountriesList>
         </div>
-        <Footer/>
+        <Footer />
       </React.Fragment>
-
     );
   }
 }
--- travel-app/src/Components/CountryCard/CountryCard.js
+++ travel-app/src/Components/CountryCard/CountryCard.js
@@ -4,12 +4,10 @@ import s from "./style.module.scss";
 class CountryCard extends React.PureComponent {
   render() {
     const { imageURL, name, capital } = this.props;
-    console.log(imageURL, name, capital);
-    // const bac = `backgroundImage: url(${imageURL});`
     return (
       <div className={s.countryCard}>
-        <div className={s.countryCard_image} style={{backgroundImage : `url(${imageURL})`}} ></div>
-        
+        <div className={s.countryCard_image} style={{ backgroundImage: `url(${imageURL})` }}></div>
+
         <h2>{name}</h2>
         <h3>{capital}</h3>
       </div>
