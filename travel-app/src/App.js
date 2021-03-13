60ae1782180895c7ce8c6b19aac9e33df32c54de
VaseninaNastya <dadasiegt@yandex.ru>
2021-03-13 16:21:17 +0300

    feat: add pages

--- travel-app/src/App.js
+++ travel-app/src/App.js
@@ -1,51 +1,15 @@
-import Test from "./Components/Test";
-import "./App.css";
-import CountryCard from "./Components/CountryCard";
-import CountriesList from "./Components/CountriesList";
-import TravelAppAPI from "./Utils/TravelAppAPI";
-import React from "react";
 
-import Footer from "./Components/Footer/Footer";
+import "./App.css";
+
+import React from "react";
+import HomePage from "./Pages/HomePage/index.js"
 
 class App extends React.PureComponent {
-  constructor() {
-    super();
-    this.travelAppAPI = new TravelAppAPI();
-  }
 
-  state = {
-    countries: [],
-  };
-
-  componentDidMount() {
-    this.loadData();
-  }
-
-  loadData = async function () {
-    const countries = await this.travelAppAPI.getCountries();
-    /* После того, как бекенд начнет отдавать данные только по одному языку,
-     * name и capital будет принимать информацию в виде
-     * name={country.capital}
-     */
-
-    this.setState({
-      countries: countries,
-    });
-  };
   render() {
-    const { countries } = this.state;
     return (
       <React.Fragment>
-        <div className="App">
-          <Test />
-          <CountriesList>
-            {countries.length === 0 ? "Data is loading..." : null}
-            {countries.map((country, index) => {
-              return <CountryCard imageURL={country.coverURL} name={country.info[0].name} capital={country.info[0].capital} key={index} />;
-            })}
-          </CountriesList>
-        </div>
-        <Footer />
+<HomePage/>
       </React.Fragment>
     );
   }
---
+++ travel-app/src/Pages/HomePage/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Test from "../../Components/Test";
+
+import CountryCard from "../../Components/CountryCard";
+import CountriesList from "../../Components/CountriesList";
+import TravelAppAPI from "../../Utils/TravelAppAPI";
+
+
+import Footer from "../../Components/Footer/Footer";
+
+
+
+
+class HomePage extends React.Component {
+    constructor() {
+        super();
+        this.travelAppAPI = new TravelAppAPI();
+      }
+    
+      state = {
+        countries: [],
+      };
+    
+      componentDidMount() {
+        this.loadData();
+      }
+    
+      loadData = async function () {
+        const countries = await this.travelAppAPI.getCountries();
+        /* После того, как бекенд начнет отдавать данные только по одному языку,
+         * name и capital будет принимать информацию в виде
+         * name={country.capital}
+         */
+    
+        this.setState({
+          countries: countries,
+        });
+      };
+      render() {
+        const { countries } = this.state;
+        return (
+          <React.Fragment>
+            <div className="App">
+              <Test />
+              <CountriesList>
+                {countries.length === 0 ? "Data is loading..." : null}
+                {countries.map((country, index) => {
+                  return <CountryCard imageURL={country.coverURL} name={country.name} capital={country.capital} key={index} />;
+                })}
+              </CountriesList>
+            </div>
+            <Footer />
+          </React.Fragment>
+        );
+      }
+}
+export default HomePage;
