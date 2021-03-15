7809844c982b539057ee511577131c85930b9f33
VaseninaNastya <dadasiegt@yandex.ru>
2021-03-15 18:10:01 +0300

    fix: add css

--- travel-app/src/Components/CountryWidget/CountryWidget.js
+++ travel-app/src/Components/CountryWidget/CountryWidget.js
@@ -7,37 +7,27 @@ class CountryWidget extends React.PureComponent {
     super();
     this.weatherAPI = new WeatherAPI();
   }
-
   state = {
     weatherData: "",
   };
-
   componentDidMount() {
     this.loadData();
   }
-
   loadData = async function () {
     const weatherData = await this.weatherAPI.getWeather(
       this.props.countryCapital
     );
-
     this.setState({
       weatherData: weatherData,
     });
   };
-
   render() {
     const { weatherData } = this.state;
-
-    console.log("weatherData", weatherData);
     let temp,description,icon= "";
-
     if (weatherData) {
       temp = `${Math.floor(weatherData.main.temp)}°C`
       description= weatherData.weather[0].description
-      console.log("description",description);
       icon =`owf-${weatherData.weather[0].id} owf`
-      console.log("icon",icon);
     }
     return (
       <div className={s.countryWidget_container}>
