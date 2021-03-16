9770aeb626bc603f113b344b2ac31025826c6d33
AV-63-dev <amanin63@mail.ru>
2021-03-17 02:23:32 +0400

    add: F11 slider

--- travel-app/src/Components/Slider/Slider.js
+++ travel-app/src/Components/Slider/Slider.js
@@ -8,69 +8,107 @@ class Slider extends Component {
     constructor(props) {
         super(props)
 
-        this.cardQuantity = 3;
-
         this.state = {
-            cardList: this.initCardList(props.attractions, this.cardQuantity),
+            attractions: props.attractions,
+            mask: [ 0, 1, 2 ],
+            maskLength: 3,
+            left: false,
+            right: false,
+            fullscreen: false,
         }
-
-        this.handlerCardList = this.handlerCardList.bind(this);
     }
-    
-    initCardList( arr, quantity ) {
-        arr.forEach( (item, index) => {
-            index < quantity ? item.displayed = true : item.displayed = false;
-        });
-        return arr;
+
+    activeBtn = (mask, arr) => {
+        const left = mask[0] === 0 ? false : true;
+        const right = mask[mask.length - 1] === arr.length - 1 ? false : true;
+        return { left, right }
+    }
+
+    initBtn = () => {
+        const arr = this.state.attractions;
+        const mask = this.state.mask;
+        const { left, right } = this.activeBtn(mask, arr);
+        this.setState({ left, right });
     };
 
-
-    handlerCardList(event) {
+    handlerCardList = event => {
         const vector = event.target.dataset.vektor;
-        const arr = this.state.cardList.concat();
-        const mask = [];
-        arr.forEach( (item, index) => item.displayed ? mask.push(index) : null );
+        const arr = this.state.attractions;
+        const mask = this.state.mask.concat();
         if (vector === 'left' && mask[0] !== 0) {
-            for (let i=0; i<mask.length; i++) {
-                mask[i] = mask[i] - 1;
+            for (let i = 0; i < mask.length; i++) mask[i] = mask[i] - 1;
+        };
+        if (vector === 'right' && mask[mask.length - 1] !== arr.length - 1 ) {
+            for (let i = 0; i < mask.length; i++) mask[i] = mask[i] + 1;
+        };
+        const { left, right } = this.activeBtn(mask, arr);
+        this.setState({ mask, left, right });
+    }
+
+    startFullscreen = event => {
+        const index = event.target.closest('.data-index').dataset.index;
+        const arr = this.state.attractions;
+        const mask = [ Number(index) ];
+        const { left, right } = this.activeBtn(mask, arr);
+        document.getElementById("slider__64bit").webkitRequestFullScreen();
+        this.setState({ mask, left, right, fullscreen: true });
+    }
+
+    stopFullscreen = () => {
+        if (!document.webkitFullscreenElement) {
+            const arr = this.state.attractions;
+            const mask = this.state.mask.concat();
+            const index = mask[0];
+            if (index === 0) {
+                for (let i = 0; i < this.state.maskLength; i++) mask[i] = i;
+            } else if (index === arr.length - 1) {
+                for (let i = 0; i < this.state.maskLength; i++) mask[i] = i + arr.length - this.state.maskLength;
+            } else {
+                for (let i = 0; i < this.state.maskLength; i++) mask[i] = i + index - 1;
             }
-            arr.forEach( (item, index) => {
-                mask.includes(index) ? item.displayed = true : item.displayed = false;
-            } );    
-            this.setState({ cardList: arr});
-        }
-        if (vector === 'right' && mask[this.cardQuantity - 1] !== arr.length - 1 ) {
-            for (let i=0; i<mask.length; i++) {
-                mask[i] = mask[i] + 1;
-            }
-            arr.forEach( (item, index) => {
-                mask.includes(index) ? item.displayed = true : item.displayed = false;
-            } );    
-            this.setState({ cardList: arr});
+            const { left, right } = this.activeBtn(mask, arr);
+            this.setState({ mask, left, right, fullscreen: false });
         }
     }
 
+    componentDidMount() {
+        this.initBtn();
+        document.addEventListener('webkitfullscreenchange', this.stopFullscreen);
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        if (nextProps.attractions !== prevState.attractions) {
+            return { attractions: nextProps.attractions,  }
+        } else { return null }
+    }
+    
     render() {
-        const cardList = this.state.cardList;
+        const { attractions, mask, left, right, fullscreen } = this.state;
 
         return (
-            <section className={s.slider}>
-                <div className={s.left} onClick={this.handlerCardList} data-vektor="left">&#9668;</div>
+            <section className={cn(s.slider, fullscreen ? s.fullscreen : null)} id='slider__64bit'>
+                <div className={cn(s.left, left ? null : s.disabled)} onClick={this.handlerCardList} data-vektor="left">&#9668;</div>
                 <div className={s.cardList}>
-                    { cardList.map( (item, index) => {
-                        if (item.displayed) { return (
-                            <div className={s.card} key={index}>
+                    { attractions.map( (item, index) => {
+                        if (mask.includes(index)) { return (
+                            <div className={`${s.card} data-index`} onClick={this.startFullscreen} data-index={index} key={index}>
                                 <p className={cn(s.title, s.cut_title)}>{ item.title }</p>
-                                <div className={s.img} style={{ backgroundImage: `url(${item.imageURL})` }} />
-                                <Rating rating={ /* item.rating */ 3.5}/>
+                                <div className={s.img} style={{ backgroundImage: `url(${item.imageURL})` }}/>
+                                { fullscreen ? 
+                                    <Rating rating={ /* item.rating */ 3.5 } onClick={() => {}} cursor={{ cursor: "pointer" }}/> :
+                                    <Rating rating={ /* item.rating */ 3.5 }/> }
                                 <p className={cn(s.description, s.cut)}>{ item.description }</p>
                             </div>
                         ) } else { return null }
                     } ) }
                 </div>
-                <div className={s.right} onClick={this.handlerCardList} data-vektor="right">&#9658;</div>
+                <div className={cn(s.right, right ? null : s.disabled)} onClick={this.handlerCardList} data-vektor="right">&#9658;</div>
             </section>
         )
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('webkitfullscreenchange', this.stopFullscreen);
     }
 }
 
