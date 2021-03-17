1835a0d8da24fa535ccc01ef46f866c49ef66837
AV-63-dev <amanin63@mail.ru>
2021-03-17 22:06:03 +0400

    upgrede scroll, width > 700 NORMA ALL

--- travel-app/src/Components/CountryInfoBlock/CountryInfoBlock.js
+++ travel-app/src/Components/CountryInfoBlock/CountryInfoBlock.js
@@ -1,5 +1,5 @@
 import React from "react";
-import s from "./style.module.scss";
+import s from "./CountryInfoBlock.module.scss";
 
 class CountryInfoBlock extends React.PureComponent {
   render() {
--- travel-app/src/Components/CountryWidget/CountryWidget.js
+++ travel-app/src/Components/CountryWidget/CountryWidget.js
@@ -1,5 +1,5 @@
 import React from "react";
-import s from "./style.module.scss";
+import s from "./CountryWidget.module.scss";
 import WeatherAPI from "../../Utils/WeatherAPI.js";
 import CountryTimer from "../CountryTimer/CountryTimer.js";
 import CountryCurrency from "../CountryCurrency/CountryCurrency.js"
